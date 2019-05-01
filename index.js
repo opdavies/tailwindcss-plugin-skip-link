b7d6354ed02433e3b1ed83ea891532cbbb5c6ba6
Oliver Davies <oliver@oliverdavies.uk>
2019-05-01 17:55:20 +0100

    Inline

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-module.exports = function () {
-  return function ({ addComponents }) {
-    addComponents([
-      {
-        '.skip-link': {
-          clip: 'rect(1px,1px,1px,1px)',
-          display: 'block',
-          height: '1px',
-          overflow: 'hidden',
-          position: 'absolute',
-          width: '1px',
-          zIndex: 999,
-          '&:focus': {
-            clip: 'auto',
-            height: 'auto',
-            overflow: 'visible',
-            width: 'auto',
-          },
+module.exports = () => ({ addComponents }) => {
+  addComponents([
+    {
+      '.skip-link': {
+        clip: 'rect(1px,1px,1px,1px)',
+        display: 'block',
+        height: '1px',
+        overflow: 'hidden',
+        position: 'absolute',
+        width: '1px',
+        zIndex: 999,
+        '&:focus': {
+          clip: 'auto',
+          height: 'auto',
+          overflow: 'visible',
+          width: 'auto',
         },
       },
-    ])
-  }
+    },
+  ])
 }
