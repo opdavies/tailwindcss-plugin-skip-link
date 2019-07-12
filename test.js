dacdb5c4eb459defcbef225d96bec8d6e7191182
Oliver Davies <oliver@oliverdavies.uk>
2019-07-12 23:31:55 +0100

    Add and use tailwindcss-plugin-test-helpers

--- test.js
+++ test.js
@@ -1,45 +1,33 @@
 const cssMatcher = require('jest-matcher-css')
-const defaultConfig = require('tailwindcss/defaultConfig')
 const plugin = require('./index.js')
-const postcss = require('postcss')
-const tailwindcss = require('tailwindcss')
-
-const generatePluginCss = () => {
-  return postcss(
-    tailwindcss({
-      corePlugins: false,
-      plugins: [plugin()]
-    })
-  )
-  .process('@tailwind components;', {
-    from: undefined
-  })
-  .then(result => result.css)
-}
+const { generateComponents } = require('tailwindcss-plugin-test-helpers')
 
 expect.extend({
-  toMatchCss: cssMatcher
+  toMatchCss: cssMatcher,
 })
 
 test('it generates classes', () => {
-  return generatePluginCss().then(css => {
-    expect(css).toMatchCss(`
-      .skip-link {
-        clip: rect(1px, 1px, 1px, 1px);
-        display: block;
-        height: 1px;
-        overflow: hidden;
-        position: absolute;
-        width: 1px;
-        z-index: 999;
-      }
+  const output = `
+    .skip-link {
+      clip: rect(1px, 1px, 1px, 1px);
+      display: block;
+      height: 1px;
+      overflow: hidden;
+      position: absolute;
+      width: 1px;
+      z-index: 999;
+    }
 
-      .skip-link:focus {
-        clip: auto;
-        height: auto;
-        overflow: visible;
-        width: auto;
-      }
-    `)
+    .skip-link:focus {
+      clip: auto;
+      height: auto;
+      overflow: visible;
+      width: auto;
+    }
+  `
+
+  generateComponents(plugin).then(result => {
+    expect(result.css).toMatchCss(output)
+    expect(result.warnings().length).toBe(0)
   })
 })
